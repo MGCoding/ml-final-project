428151da3ff6ec6a00228478c142d16732806261
flaming.zelda <flaming.zelda@gmail.com>
2013-11-20 21:11:10 -0600

    Added Decision Trees.  Work in progress.

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-train.o:
-	g++ -c src/train.cpp -o obj/train.o
+model.o:
+	g++ -c src/model.cpp -o obj/model.o -O3
 
-train: train.o
-	g++ obj/train.o -o bin/train
+GNB.o:
+	g++ -c src/GNB.cpp -o obj/GNB.o -O3
+	
+DT.o:
+	g++ -c src/DT.cpp -o obj/DT.o -O3
+	
+GNB-test: GNB.o model.o
+	g++ obj/GNB.o obj/model.o src/GNB-test.cpp -o bin/GNB-test -O3
+	
+DT-test: DT.o model.o
+	g++ obj/DT.o obj/model.o src/DT-test.cpp -o bin/DT-test -O3
 	
 clean:
 	rm obj/*
--- src/GNB.h
+++ src/GNB.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <cmath>
+#include <float.h>
 #include "model.h"
 
 using namespace std;
@@ -13,8 +15,9 @@ private:
 	//mu indexed by sensor then hashed by annotation
 	vector<map<string, long double> > mu;
 	vector<map<string, long double> > sigma;
+	static const double SQRT_2PI = 2.50662827463;
 public:
 	void computeMu(vector<person> & data);
 	void computeSigma(vector<person> & data);
-
+	string computeAnnotation(double * sensorData);
 };
